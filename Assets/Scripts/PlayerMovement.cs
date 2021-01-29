fa5497e87efeb41e996df6772edf2610a1424430
ahoerack03 <anna.hoerack2003@gmail.com>
2021-01-29 16:32:55 +0100

    update design and character movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,6 +9,11 @@ public class PlayerMovement : MonoBehaviour
     public Vector3 worldPos;
     public Vector3 playerPos;
     public float speed = 0.1f;
+    public float minScale;
+    public float maxScale;
+    public float scale;
+    public Transform minHeight;
+    public Transform maxHeight;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,6 +24,10 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
        thisTransform.position = Vector3.MoveTowards(thisTransform.position, playerPos, speed);
+       float value = Mathf.InverseLerp(minHeight.position.y, maxHeight.position.y, thisTransform.position.y);
+       scale = Mathf.Lerp(minScale, maxScale, value);
+       Vector3 scaleVector = new Vector3 (scale,scale,scale);
+       thisTransform.localScale = scaleVector;
     }
     public void SetNewDestination()
     {
